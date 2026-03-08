0cea31659f3f2c61d6fd8d8e430b9b967236e896
Mainur <kentykimainur@gmail.com>
2026-03-08 13:25:12 +0600

    create a first component

---
+++ src/components/CartsTable/CartsTable.tsx
@@ -0,0 +1,55 @@
+import React from 'react'
+import type { Cart } from '../../types/cart'
+import { Link } from "react-router-dom"
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import Button from '@mui/material/Button';
+
+interface Props {
+  carts: Cart[];
+}
+
+export default function CartsTable({ carts }: Props) {
+  return (
+    <Table>
+        <TableHead>
+            <TableRow>
+                <TableCell>Cart ID</TableCell>
+                <TableCell>User ID</TableCell>
+                <TableCell>Total</TableCell>
+                <TableCell>Discount Total</TableCell>
+                <TableCell>Actions</TableCell>
+            </TableRow>
+        </TableHead>
+        <TableBody>
+            {carts.map(cart => {
+                const discount = cart.total - cart.discountTotal;
+                return (
+                    <TableRow key={cart.id}>
+                        <TableCell>{cart.id}</TableCell>
+                        <TableCell>{cart.total}$</TableCell>
+                        <TableCell>{discount}$</TableCell>
+                        <TableCell>{cart.total}</TableCell>
+                        
+                        <TableCell>
+                            <Button component={Link} to={`/carts/${cart.id}`} variant="contained" color="primary">
+                                View Details
+                            </Button>
+                        </TableCell>
+
+                        <TableCell>
+                            <Button component={Link} to={`/users/${cart.userId}`} variant="outlined" color="secondary">
+                                Profile
+                            </Button>
+                        </TableCell>
+
+                    </TableRow>
+                );
+            })}
+        </TableBody>
+    </Table>
+  )
+}
